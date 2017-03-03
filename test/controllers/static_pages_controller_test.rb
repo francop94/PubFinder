e18377f74ea7d48d0f5b4920d8df7a48a0245c24
mychemicalro <myrchemical@mail.com>
2017-03-03 21:44:31 +0100

    css

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -6,11 +6,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 		@base_title="Ruby on Rails Tutorial Sample App"
 	end
 
-  test "should get home without home" do
-    get static_pages_home_url
-    assert_response :success
-    assert_select "title", "Ruby on Rails Tutorial Sample App"
-  end
+
 
   test "should get home" do
     get static_pages_home_url
