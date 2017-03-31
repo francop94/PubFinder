8ce699496d06ddb3ab49e633efd35efcbd686b65
mychemicalro <myrchemical@mail.com>
2017-03-31 20:50:48 +0200

    finish user edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,16 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update, :index]
+  before_action :logged_in_user, only: [:edit, :update, :index, :destroy]
   before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
 
   def index
     @users = User.paginate(page: params[:page])
+  end
 
-
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
   end
 
 
@@ -62,6 +67,10 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)  
   	end
+
+  	def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
 
 
 end
